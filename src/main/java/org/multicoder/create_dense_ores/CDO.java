9dcc0a3e8537f1b86536a550156e6bbb92e23a36
Multi-Coder-24 <gtavster77@gmail.com>
2026-09-17 08:10:13 +0100

    Version 1.0.1 • Fixed data files to ignore optional mod if not present. • Fixed block registry to only register blocks for loaded mods
    Signed-off-by: Multi-Coder-24 <51382988+multi-coder-24@users.noreply.github.com>

--- src/main/java/org/multicoder/create_dense_ores/CDO.java
+++ src/main/java/org/multicoder/create_dense_ores/CDO.java
@@ -5,6 +5,7 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.multicoder.create_dense_ores.core.CDOBlocks;
 import org.multicoder.create_dense_ores.core.CDOItems;
@@ -20,21 +21,38 @@ public class CDO {
         CDOBlocks.BLOCKS.register(modEventBus);
         CDOItems.ITEMS.register(modEventBus);
         modEventBus.addListener(this::AppendCreativeTab);
+        modEventBus.addListener(this::Construct);
         LOGGER.info("Create Dense Ores Initialised");
     }
+
+    public void Construct(FMLConstructModEvent event){
+        if(Utility.isModLoaded("tfmg")){
+            LOGGER.info("Create: The Factory Must Grow Found, Adding Dense Ores");
+            CDOBlocks.loadTFMG();
+        }
+        if(Utility.isModLoaded("create_new_age")){
+            LOGGER.info("Create: New Age Found, Adding Dense Ores");
+            CDOBlocks.loadNewAge();
+        }
+    }
+
 
     public void AppendCreativeTab(BuildCreativeModeTabContentsEvent event){
         if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS){
             event.accept(CDOBlocks.DEEPSLATE_DENSE_ZINC_ORE.get());
             event.accept(CDOBlocks.DENSE_ZINC_ORE.get());
-            event.accept(CDOBlocks.DEEPSLATE_DENSE_THORIUM_ORE.get());
-            event.accept(CDOBlocks.DENSE_THORIUM_ORE.get());
-            event.accept(CDOBlocks.DEEPSLATE_DENSE_LEAD_ORE.get());
-            event.accept(CDOBlocks.DENSE_LEAD_ORE.get());
-            event.accept(CDOBlocks.DEEPSLATE_DENSE_LITHIUM_ORE.get());
-            event.accept(CDOBlocks.DENSE_LITHIUM_ORE.get());
-            event.accept(CDOBlocks.DEEPSLATE_DENSE_NICKEL_ORE.get());
-            event.accept(CDOBlocks.DENSE_NICKEL_ORE.get());
+            if(Utility.isModLoaded("tfmg")){
+                event.accept(CDOBlocks.DEEPSLATE_DENSE_LEAD_ORE.get());
+                event.accept(CDOBlocks.DENSE_LEAD_ORE.get());
+                event.accept(CDOBlocks.DEEPSLATE_DENSE_LITHIUM_ORE.get());
+                event.accept(CDOBlocks.DENSE_LITHIUM_ORE.get());
+                event.accept(CDOBlocks.DEEPSLATE_DENSE_NICKEL_ORE.get());
+                event.accept(CDOBlocks.DENSE_NICKEL_ORE.get());
+            }
+            if(Utility.isModLoaded("create_new_age")){
+                event.accept(CDOBlocks.DEEPSLATE_DENSE_THORIUM_ORE.get());
+                event.accept(CDOBlocks.DENSE_THORIUM_ORE.get());
+            }
         }
     }
 }
--- src/main/java/org/multicoder/create_dense_ores/core/CDOBlocks.java
+++ src/main/java/org/multicoder/create_dense_ores/core/CDOBlocks.java
@@ -14,19 +14,34 @@ public class CDOBlocks {
 
     public static final DeferredBlock<?> DENSE_ZINC_ORE = register("dense_zinc_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops());
     public static final DeferredBlock<?> DEEPSLATE_DENSE_ZINC_ORE = register("deepslate_dense_zinc_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops());
-    public static DeferredBlock<?> DENSE_LITHIUM_ORE = register("dense_lithium_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DEEPSLATE_DENSE_LITHIUM_ORE = register("deepslate_dense_lithium_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DENSE_LEAD_ORE = register("dense_lead_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DEEPSLATE_DENSE_LEAD_ORE = register("deepslate_dense_lead_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DENSE_NICKEL_ORE = register("dense_nickel_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DEEPSLATE_DENSE_NICKEL_ORE = register("deepslate_dense_nickel_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
-    public static DeferredBlock<?> DENSE_THORIUM_ORE = register("dense_thorium_ore",BlockBehaviour.Properties.of().strength(3.5F).requiresCorrectToolForDrops());
-    public static DeferredBlock<?> DEEPSLATE_DENSE_THORIUM_ORE = register("deepslate_dense_thorium_ore",BlockBehaviour.Properties.of().strength(3.5F).requiresCorrectToolForDrops());
+    public static DeferredBlock<?> DENSE_LITHIUM_ORE;
+    public static DeferredBlock<?> DEEPSLATE_DENSE_LITHIUM_ORE;
+    public static DeferredBlock<?> DENSE_LEAD_ORE;
+    public static DeferredBlock<?> DEEPSLATE_DENSE_LEAD_ORE;
+    public static DeferredBlock<?> DENSE_NICKEL_ORE;
+    public static DeferredBlock<?> DEEPSLATE_DENSE_NICKEL_ORE;
+    public static DeferredBlock<?> DENSE_THORIUM_ORE;
+    public static DeferredBlock<?> DEEPSLATE_DENSE_THORIUM_ORE;
 
 
     public static DeferredBlock<?> register(String name,BlockBehaviour.Properties properties) {
         DeferredBlock<?> returnValue = BLOCKS.registerBlock(name,Block::new, properties);
         CDOItems.ITEMS.registerSimpleBlockItem(returnValue);
         return returnValue;
+    }
+
+    public static void loadTFMG() {
+        DENSE_LITHIUM_ORE = register("dense_lithium_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+        DEEPSLATE_DENSE_LITHIUM_ORE = register("deepslate_dense_lithium_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+        DENSE_LEAD_ORE = register("dense_lead_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+        DEEPSLATE_DENSE_LEAD_ORE = register("deepslate_dense_lead_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+        DENSE_NICKEL_ORE = register("dense_nickel_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+        DEEPSLATE_DENSE_NICKEL_ORE = register("deepslate_dense_nickel_ore",BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_GOLD_ORE).requiresCorrectToolForDrops().sound(SoundType.STONE));
+
+    }
+
+    public static void loadNewAge() {
+        DENSE_THORIUM_ORE = register("dense_thorium_ore",BlockBehaviour.Properties.of().strength(3.5F).requiresCorrectToolForDrops());
+        DEEPSLATE_DENSE_THORIUM_ORE = register("deepslate_dense_thorium_ore",BlockBehaviour.Properties.of().strength(3.5F).requiresCorrectToolForDrops());
     }
 }
